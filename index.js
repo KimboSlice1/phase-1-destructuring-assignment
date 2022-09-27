02c700291d2d175a0558b67bce39ecfcf3e59c58
Kimber Herron <kh582013@gmail.com>
2022-09-27 15:41:55 -0400

    commit tree aa80a0184b4c813c927cde746ba3b31fd800dbf1

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const farmAnimals = 'cow horse sheep pig chicken';
+const farmAnimals = ['cow, horse, sheep, pig, chicken']
+const {moo, neigh, baa, oink, cluck} = farmAnimals
+cow;
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
